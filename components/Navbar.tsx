c870f5ef20a08fa3ea169b1e54aeb8d70ce56f13
DarKS4ys <melihyardim1057@gmail.com>
2023-12-06 00:54:13 +0300

    mobile nav

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,10 +8,14 @@ import { clsx } from 'clsx';
 import UserMenuButton from './Auth/UserMenuButton'
 import type { Session } from 'next-auth'
 import Link from 'next/link';
-import { useRouter } from 'next/navigation'
+import { usePathname, useRouter } from 'next/navigation'
+import MobileSheet from './MobileSheet'
 
 
 export default function Navbar({navbar, lang, session}: {navbar: any, lang: Locale, session: Session | null}) {
+
+  const pathname = usePathname();
+  const pathnameWithoutLanguage = pathname.replace(/^\/[a-z]{2}\//, '');
 
     const options = navbar.options;
 
@@ -36,12 +40,16 @@ export default function Navbar({navbar, lang, session}: {navbar: any, lang: Loca
   return (
     <div className={clsx('z-50 px-8 flex items-center justify-center sticky top-0 border-b border-border bg-background/90 backdrop-blur-md transition-all duration-300', color ? 'py-2 transition-all' : 'py-4')}>
         <div className='max-w-7xl justify-between items-center flex w-full'>
-          <Link href="/" className='text-xl font-medium'>Türkiye Rehberim</Link>
+          <Link href="/" className='text-xl font-medium md:block hidden'>Türkiye Rehberim</Link>
 
-          <div className='flex items-center gap-3'>
+          <li className='md:hidden flex'>
+            <MobileSheet navbar={navbar} lang={lang} pathnameWithoutLanguage={pathnameWithoutLanguage}/>
+          </li>
+
+          <div className='hidden md:flex items-center gap-3'>
             {Object.entries(options).map(([key, value]) => (
               <Link 
-              href={key === '/admin/create-place' ? '/create-place' : key}
+              href={pathnameWithoutLanguage === key ? `/${lang}` : key === 'contact' ? `/${lang}/#${key}` : `/${lang}/${key}`}
               key={key}>
                 <p className='px-3 py-2 hover:bg-highlighthover hover:scale-110 active:scale-95 hover:text-white rounded-xl transition duration-200 text-neutral-700 dark:text-neutral-300 dark:hover:text-white'>
                   {typeof value === 'string' ? value : null}
