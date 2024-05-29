9074d630f8cbd828226fc941495f48448c90b79a
DarKS4ys <melihyardim1057@gmail.com>
2024-05-29 15:08:20 +0300

    update

--- app/[lang]/assistantv2/page.tsx
+++ app/[lang]/assistantv2/page.tsx
@@ -1,5 +1,7 @@
+"use client"
+
 import BorderButton from "@/components/ui/border-button";
-import React from "react";
+import React, { useEffect } from "react";
 import StartButton from "./start-button";
 
 export default function page() {
--- app/[lang]/assistantv2/start-button.tsx
+++ app/[lang]/assistantv2/start-button.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React, { useEffect, useRef, useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 
 declare global {
   interface Window {
@@ -13,54 +14,108 @@ export default function StartButton() {
   const [recordingComplete, setRecordingComplete] = useState(false);
   const [transcript, setTranscript] = useState("");
 
-  const recognitionRef = useRef<any>()
+  const recognitionRef = useRef<any>();
 
   useEffect(() => {
     return () => {
       if (recognitionRef.current) {
-        recognitionRef.current.stop()
+        recognitionRef.current.stop();
       }
-    }
-  }, [])
+    };
+  }, []);
 
   const startRecording = () => {
     setIsRecording(true);
-    recognitionRef.current = new window.webkitSpeechRecognition()
-    recognitionRef.current.continuous = true
-    recognitionRef.current.interimResults = true
-    recognitionRef.current.lang = 'tr-TR'; // ! delete after
+    recognitionRef.current = new window.webkitSpeechRecognition();
+    recognitionRef.current.continuous = true;
+    recognitionRef.current.interimResults = true;
+    recognitionRef.current.lang = "tr-TR"; // ! delete after
     recognitionRef.current.onresult = (event: any) => {
-      const {transcript} = event.results[event.results.length - 1][0]
-      setTranscript(transcript)
-    }
+      const { transcript } = event.results[event.results.length - 1][0];
+      setTranscript(transcript);
+    };
 
-    recognitionRef.current.start()
+    recognitionRef.current.start();
   };
 
   const stopRecording = () => {
     if (recognitionRef.current) {
-      recognitionRef.current.stop()
+      recognitionRef.current.stop();
       setIsRecording(!isRecording);
-      
     }
   };
 
   const handleToggleRecording = () => {
-    setIsRecording(!isRecording)
+    setIsRecording(!isRecording);
     if (!isRecording) {
-      startRecording()
+      startRecording();
     } else {
-      stopRecording()
+      stopRecording();
     }
-  }
+  };
+
+  // add
+  const fadeInAnimationVariants = {
+    // for framer motion
+    initial: {
+      opacity: 0,
+      y: 100,
+    },
+    animate: (index: number) => ({
+      opacity: 1,
+      y: 0,
+      transition: {
+        delay: 0.05 * index,
+      },
+    }),
+  };
 
   return (
     <>
-      <button onClick={handleToggleRecording} className="bg-primary active:scale-95 hover:scale-125 
-      hover:bg-primary/80 transition duration-200 flex items-center font-bold text-3xl justify-center 
-      rounded-full p-8 text-primary-foreground  aspect-square">
-        {!isRecording ? 'BAŞLA' : 'KONUŞ...'}
-      </button>
+      <div className="h-44 flex items-center justify-center">
+        <AnimatePresence>
+          {!isRecording && (
+            <motion.button
+              exit={{ scaleX: 2, scaleY: 0.4, opacity: 0 }}
+              initial={{ scaleY: 1 }}
+              whileHover={{ scale: 1.2 }}
+              whileTap={{ scale: 0.9 }}
+              transition={{
+                type: "spring",
+                damping: 17,
+                stiffness: 150,
+                duration: 1,
+              }}
+              onClick={handleToggleRecording}
+              className="absolute bg-primary 
+              hover:bg-primary/80 transition-color flex items-center font-bold text-3xl justify-center 
+              rounded-full p-8 text-primary-foreground  aspect-square"
+            >
+              BAŞLA
+            </motion.button>
+          )}
+        </AnimatePresence>
+
+        <AnimatePresence>
+          {isRecording && (
+            <div className="flex items-center gap-x-2">
+              {Array.from({ length: 5 }, (_, index) => (
+                <motion.div
+                  key={index}
+                  custom={index}
+                  variants={fadeInAnimationVariants}
+                  initial="initial"
+                  whileInView="animate"
+                  viewport={{
+                    once: true,
+                  }}
+                  className="bg-primary h-28 w-24 rounded-full"
+                />
+              ))}
+            </div>
+          )}
+        </AnimatePresence>
+      </div>
 
       {transcript && (
         <div className="border text-primary rounded-md p-2 mt-4">
